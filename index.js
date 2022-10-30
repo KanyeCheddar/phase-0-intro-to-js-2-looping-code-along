9bb50ba8dd2e0fb1e1b8a6840985d5a0ba7dcee7
Akeem Lawrence <akeemwlawrence@gmail.com>
2022-10-30 06:11:47 -0400

    update

--- index.js
+++ index.js
@@ -15,13 +15,3 @@ function countDown( number ) {
     }
     console.log(number);
 };
-
-/*
-function countDown( startingNumber ) {
-    while ( startingNumber > 0 ) {
-      console.log( startingNumber );
-      startingNumber -= 1;
-    }
-    console.log( startingNumber );
-  }
-  */
